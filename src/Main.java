ea2da6835b0e4fa80a2cf739da36a3db52484437
Janek Tuisk <38907917+jTuisk@users.noreply.github.com>
2020-10-06 11:46:39 +0300

    push 06.10.2020

--- src/Main.java
+++ src/Main.java
@@ -7,14 +7,29 @@ public class Main {
         String name = "N/A";
         System.out.println("Kirjoita nimesi:");
         name = scanner.nextLine();
+        name = cleanUpName(name);
         System.out.println(reverseName(name));
+        System.out.println(checkPalindrome(name));
     }
 
+    static String cleanUpName(String name){
+        String cleanName = "";
+        name.toLowerCase();
+        cleanName = name.replaceAll("[^a-zA-Z -]", "");
+        return cleanName;
+    }
     static String reverseName(String name){
         String reversedName = "";
         for(int i = name.length()-1; name.length() != reversedName.length(); i--){
             reversedName += name.charAt(i);
         }
         return reversedName;
+    }
+
+    static String checkPalindrome(String name){
+        if(name.equals(reverseName(name)))
+            return "Nimi on palindromi";
+
+        return "Nimi ei ole palindromi";
     }
 }
